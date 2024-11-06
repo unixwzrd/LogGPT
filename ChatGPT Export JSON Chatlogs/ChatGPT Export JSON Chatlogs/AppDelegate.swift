6ca2744dd92a0945c71cf2b7c09897b45a546554
M S <unixwzrd.register@mac.com>
2024-11-06 12:22:28 -0600

    Well, it worked before I upgraded to 15.1

---
+++ ChatGPT Export JSON Chatlogs/ChatGPT Export JSON Chatlogs/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ChatGPT Export JSON Chatlogs
+//
+//  Created by Michael Sullivan on 11/4/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ ChatGPT Export JSON Chatlogs/ChatGPT Export JSON ChatlogsTests/ChatGPT_Export_JSON_ChatlogsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_Export_JSON_ChatlogsTests.swift
+//  ChatGPT Export JSON ChatlogsTests
+//
+//  Created by Michael Sullivan on 11/4/24.
+//
+
+import Testing
+@testable import ChatGPT_Export_JSON_Chatlogs
+
+struct ChatGPT_Export_JSON_ChatlogsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
